efb11553c719faae74b6dd0d277a94d13871f8fe
bertablancpastor <72865106+bertablancpastor@users.noreply.github.com>
2023-07-03 11:41:46 +0000

    dia1

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -2,12 +2,66 @@ const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
 			//Your data structures, A.K.A Entities
+			contacts: []
 		},
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
+
+			//1.Get All available agendas right now
+			getContacts: () => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/bertablancpastoragenda/", {
+					method: "GET",
+				})
+					.then((response) => response.json())
+					.then((data) => setStore({ contacts: data }))// => guardo el json en un espacio de memoria
+					.catch((error) => console.log(error))// => te aviso si algo sale mal
+			},
+			
+			//7.Create one contact
+			createContact: (full_name, email, address, phone) => {
+				fetch('https://assets.breatheco.de/apis/fake/contact', {
+					method: 'POST',
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						"full_name": full_name,
+						"email": email,
+						"agenda_slug": "bertablancpastoragenda",
+						"address": address,
+						"phone": phone
+					}), // body data type must match "Content-Type" header
+				})//busca informacion a la url dada con el metodo especificado
+					.then((response) => response.json())// => convierto la respuesta buscada en un json => {"info":{},"results":[]} "hola"
+					.then((data) => console.log(data))// => guardo el json en un espacio de memoria
+					.catch((error) => console.log(error))// => te aviso si algo sale mal
+			},
+
+			//8.Update one contact
+			updateContact: (full_name, email, address, phone) => {
+				fetch('https://assets.breatheco.de/apis/fake/contact', {
+					method: 'PUT',
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						"full_name": full_name,
+						"email": email,
+						"agenda_slug": "bertablancpastoragenda",
+						"address": address,
+						"phone": phone
+					}), // body data type must match "Content-Type" header
+				})//busca informacion a la url dada con el metodo especificado
+					.then((response) => response.json())// => convierto la respuesta buscada en un json => {"info":{},"results":[]} "hola"
+					.then((data) => console.log(data))// => guardo el json en un espacio de memoria
+					.catch((error) => console.log(error))// => te aviso si algo sale mal
+
+			},
+
 		}
-	};
-};
+	}
+}
+
 
 export default getState;
--- src/js/views/Contacts.js
+++ src/js/views/Contacts.js
@@ -1,13 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 
 import { ContactCard } from "../component/ContactCard.js";
 import { Modal } from "../component/Modal";
+import { Context } from "../store/appContext.js";
 
 export const Contacts = () => {
 	const [state, setState] = useState({
 		showModal: false
 	});
+
+	 const {store, actions} = useContext(Context)
+	 const [contacts, setContacts] = useState(store.contacts)
 
 	return (
 		<div className="container">
@@ -17,14 +21,16 @@ export const Contacts = () => {
 						Add new contact
 					</Link>
 				</p>
-				<div id="contacts" className="panel-collapse collapse show" aria-expanded="true">
+				{/* <div id="contacts" className="panel-collapse collapse show" aria-expanded="true">
 					<ul className="list-group pull-down" id="contact-list">
 						<ContactCard onDelete={() => setState({ showModal: true })} />
 						<ContactCard />
 						<ContactCard />
 						<ContactCard />
 					</ul>
-				</div>
+				</div> */}
+				<ul>{contacts.map((contact) => (<ContactCard  key={contact.id} full_name={contact.full_name} email={contact.email} address={contact.address} phone={contact.phone}/>) )}</ul>
+				
 			</div>
 			<Modal show={state.showModal} onClose={() => setState({ showModal: false })} />
 		</div>
