4cde12f7150cbe95686c446445df79df67f79f56
bertablancpastor <72865106+bertablancpastor@users.noreply.github.com>
2023-07-04 14:33:40 +0000

    aplicado crear contacto

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,9 +1,11 @@
 const getState = ({ getStore, setStore }) => {
 	return {
+		//Donde almazenamos los datos
 		store: {
 			//Your data structures, A.K.A Entities
 			contacts: []
 		},
+		//Donde se crean las funciones que se iran ejecutando globalmente
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
@@ -14,30 +16,11 @@ const getState = ({ getStore, setStore }) => {
 					method: "GET",
 				})
 					.then((response) => response.json())
+					.then((response) => console.log(response))
 					.then((data) => setStore({ contacts: data }))// => guardo el json en un espacio de memoria
 					.catch((error) => console.log(error))// => te aviso si algo sale mal
 			},
 			
-			//7.Create one contact
-			createContact: (full_name, email, address, phone) => {
-				fetch('https://assets.breatheco.de/apis/fake/contact', {
-					method: 'POST',
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({
-						"full_name": full_name,
-						"email": email,
-						"agenda_slug": "bertablancpastoragenda",
-						"address": address,
-						"phone": phone
-					}), // body data type must match "Content-Type" header
-				})//busca informacion a la url dada con el metodo especificado
-					.then((response) => response.json())// => convierto la respuesta buscada en un json => {"info":{},"results":[]} "hola"
-					.then((data) => console.log(data))// => guardo el json en un espacio de memoria
-					.catch((error) => console.log(error))// => te aviso si algo sale mal
-			},
-
 			//8.Update one contact
 			updateContact: (full_name, email, address, phone) => {
 				fetch('https://assets.breatheco.de/apis/fake/contact', {
@@ -56,7 +39,6 @@ const getState = ({ getStore, setStore }) => {
 					.then((response) => response.json())// => convierto la respuesta buscada en un json => {"info":{},"results":[]} "hola"
 					.then((data) => console.log(data))// => guardo el json en un espacio de memoria
 					.catch((error) => console.log(error))// => te aviso si algo sale mal
-
 			},
 
 		}
--- src/js/views/AddContact.js
+++ src/js/views/AddContact.js
@@ -1,29 +1,60 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export const AddContact = () => {
+
+	const [contact, setContact] = useState({
+		"full_name": "",
+		"email": "",
+		"agenda_slug": "bertablancpastoragenda",
+		"address": "",
+		"phone": ""
+	})
+
+	const navigate = useNavigate()
+
+	//Crear contacto
+	const createContact = (event) => {
+		event.preventDefault()
+		fetch('https://assets.breatheco.de/apis/fake/contact', {
+			method: 'POST',
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(contact), // body data type must match "Content-Type" header
+		})//busca informacion a la url dada con el metodo especificado
+			.then((response) => response.json())// => convierto la respuesta buscada en un json => {"info":{},"results":[]} "hola"
+			.then((data) => console.log(data))// => guardo el json en un espacio de memoria
+			.then((response) => navigate("/"))
+			.catch((error) => console.log(error))// => te aviso si algo sale mal
+	}
+
+	const handleChange = (event) => {
+		setContact({...contact, [event.target.name] : event.target.value})		
+	}
+
 	return (
 		<div className="container">
 			<div>
 				<h1 className="text-center mt-5">Add a new contact</h1>
-				<form>
+				<form onSubmit={createContact}>
 					<div className="form-group">
 						<label>Full Name</label>
-						<input type="text" className="form-control" placeholder="Full Name" />
+						<input type="text" onChange={handleChange} className="form-control" placeholder="Full Name" name="full_name" />
 					</div>
 					<div className="form-group">
 						<label>Email</label>
-						<input type="email" className="form-control" placeholder="Enter email" />
+						<input type="email" onChange={handleChange} className="form-control" placeholder="Enter email" name="email" />
 					</div>
 					<div className="form-group">
 						<label>Phone</label>
-						<input type="phone" className="form-control" placeholder="Enter phone" />
+						<input type="phone" onChange={handleChange} className="form-control" placeholder="Enter phone" name="phone" />
 					</div>
 					<div className="form-group">
 						<label>Address</label>
-						<input type="text" className="form-control" placeholder="Enter address" />
+						<input type="text" onChange={handleChange} className="form-control" placeholder="Enter address" name="address" />
 					</div>
-					<button type="button" className="btn btn-primary form-control">
+					<button type="submit"  className="btn btn-primary form-control">
 						save
 					</button>
 					<Link className="mt-3 w-100 text-center" to="/">
